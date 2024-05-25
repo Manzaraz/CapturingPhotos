441602dbb653003186abeb552efa342ee90ed3c9
Christian Manzaraz <magnusmanz@gmail.com>
2024-05-25 13:35:14 -0300

    Initial Commit

---
+++ CapturingPhotos/CapturingPhotosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapturingPhotosApp.swift
+//  CapturingPhotos
+//
+//  Created by Christian Manzaraz on 25/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct CapturingPhotosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
